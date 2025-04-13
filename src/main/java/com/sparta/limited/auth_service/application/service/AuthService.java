6df89c11330ee825f1ea42f17f33659c33f8d7ca
Imnotcoderdude <topsolo7@naver.com>
2025-04-14 00:40:20 +0900

    ♻️ refactor [#2] : AuthService에서 직접 FeignClient 호출 부분 변경 - AuthService에서 직접 FeignClient를 호출하는 것이 아닌 FeignClient를 호출하는 서비스 클래스를 만들고 해당 클래스를 DI 받아서 사용. - FeignClient의 예외처리나 호출 부분을 AuthService에서 처리하는 것이 아닌 FeignClient연결 서비스 클래스에서 처리하도록 책임 분리

--- src/main/java/com/sparta/limited/auth_service/application/service/AuthService.java
+++ src/main/java/com/sparta/limited/auth_service/application/service/AuthService.java
@@ -3,9 +3,9 @@ package com.sparta.limited.auth_service.application.service;
 import com.sparta.limited.auth_service.application.dto.request.AuthSignupRequest;
 import com.sparta.limited.auth_service.application.dto.response.AuthSignupResponse;
 import com.sparta.limited.auth_service.application.mapper.AuthDtoMapper;
+import com.sparta.limited.auth_service.application.service.user.UserFeignService;
 import com.sparta.limited.auth_service.infrastructure.dto.request.UserCreateInternalRequest;
 import com.sparta.limited.auth_service.infrastructure.dto.response.UserCreateInternalResponse;
-import com.sparta.limited.auth_service.infrastructure.feign.UserFeignClient;
 import com.sparta.limited.auth_service.infrastructure.password.PasswordEncoderUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +14,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final UserFeignClient userFeignClient;
+    private final UserFeignService userFeignService;
 
     public AuthSignupResponse signup(AuthSignupRequest request) {
         String password = PasswordEncoderUtil.encode(request.getPassword());
         UserCreateInternalRequest createUserRequest = AuthDtoMapper.toCreateUserInternalDto(
             request,
             password);
-        UserCreateInternalResponse response = userFeignClient.createUser(createUserRequest);
+        UserCreateInternalResponse response = userFeignService.createUser(createUserRequest);
         return AuthDtoMapper.toSignupExternalResponse(response);
     }
 
