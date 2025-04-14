a339943fe7a603e04fc15440bf520d2671e3b0be
Imnotcoderdude <topsolo7@naver.com>
2025-04-14 15:19:22 +0900

    ✨ feat [#2] : FeignClient 호출하여 데이터 받아오는 서비스 로직 작성  - 해당 기능은 비밀번호 검증을 하는 auth 기능이 아닌 FeignClient를 사용하여 데이터를 주고 받는 기능이기 때문에 UserFeignService에 작성  - 받아온 데이터를 UserInfo로 변환

--- src/main/java/com/sparta/limited/auth_service/infrastructure/service/UserFeignService.java
+++ src/main/java/com/sparta/limited/auth_service/infrastructure/service/UserFeignService.java
@@ -1,10 +1,13 @@
 package com.sparta.limited.auth_service.infrastructure.service;
 
+import com.sparta.limited.auth_service.application.dto.request.AuthLoginRequest;
 import com.sparta.limited.auth_service.application.dto.request.AuthSignupRequest;
 import com.sparta.limited.auth_service.application.dto.response.AuthSignupResponse;
+import com.sparta.limited.auth_service.application.info.UserInfo;
 import com.sparta.limited.auth_service.application.mapper.AuthDtoMapper;
 import com.sparta.limited.auth_service.infrastructure.dto.request.UserCreateInternalRequest;
 import com.sparta.limited.auth_service.infrastructure.dto.response.UserCreateInternalResponse;
+import com.sparta.limited.auth_service.infrastructure.dto.response.UserSearchInternalResponse;
 import com.sparta.limited.auth_service.infrastructure.feign.UserFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,4 +26,17 @@ public class UserFeignService {
         return AuthDtoMapper.toSignupExternalResponse(userCreateResponse);
     }
 
+    public UserInfo searchUserByUsername(AuthLoginRequest request) {
+        UserSearchInternalResponse userSearchResponse = userFeignClient.searchUserByUsername(
+            request.getUsername());
+        return UserInfo.from(
+            userSearchResponse.getUserId(),
+            userSearchResponse.getUsername(),
+            userSearchResponse.getPassword(),
+            userSearchResponse.getRole(),
+            userSearchResponse.getGender(),
+            userSearchResponse.getAge(),
+            userSearchResponse.getAddress()
+        );
+    }
 }
