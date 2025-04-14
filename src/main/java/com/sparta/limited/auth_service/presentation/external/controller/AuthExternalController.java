aec4f6d0444337207440298f86770ed73b1b50e3
Imnotcoderdude <topsolo7@naver.com>
2025-04-14 15:33:19 +0900

    ✨ feat [#2] : 로그인 API 컨트롤러 추가  - 클라이언트에게 usernmae과 password를 받음  - 쿠키 반환을 위한 HttpServletResponse 파라미터에 추가  - 로그인 기능 호출

--- src/main/java/com/sparta/limited/auth_service/presentation/external/controller/AuthExternalController.java
+++ src/main/java/com/sparta/limited/auth_service/presentation/external/controller/AuthExternalController.java
@@ -1,8 +1,11 @@
 package com.sparta.limited.auth_service.presentation.external.controller;
 
+import com.sparta.limited.auth_service.application.dto.request.AuthLoginRequest;
 import com.sparta.limited.auth_service.application.dto.request.AuthSignupRequest;
+import com.sparta.limited.auth_service.application.dto.response.AuthLoginResponse;
 import com.sparta.limited.auth_service.application.dto.response.AuthSignupResponse;
 import com.sparta.limited.auth_service.application.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +23,19 @@ public class AuthExternalController {
 
     @PostMapping("/signup")
     public ResponseEntity<AuthSignupResponse> signup(
-        @RequestBody AuthSignupRequest request) {
+        @RequestBody AuthSignupRequest request
+    ) {
         AuthSignupResponse response = authService.signup(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthLoginResponse> login(
+        @RequestBody AuthLoginRequest request,
+        HttpServletResponse servletResponse
+    ) {
+        AuthLoginResponse response = authService.login(servletResponse, request);
+        return ResponseEntity.ok(response);
     }
 
 }
