53709521f364561cce59dc114159ffa62fd3ac08
Imnotcoderdude <topsolo7@naver.com>
2025-04-14 15:13:57 +0900

    🩹 fix [#2] : internal DTO 클래스 수정  - 사용자 검색은 username 기반 RequestParam으로 받기 떄문에 request DTO가 필요 없다고 생각하여 삭제  - user-service로부터 받아온 데이터에 password가 포함되어 있어야 비밀번호 검사가 가능하기때문에 password 받아오도록 필드 추가

--- src/main/java/com/sparta/limited/auth_service/infrastructure/dto/response/UserSearchInternalResponse.java
+++ src/main/java/com/sparta/limited/auth_service/infrastructure/dto/response/UserSearchInternalResponse.java
@@ -7,27 +7,43 @@ public class UserSearchInternalResponse {
 
     private final Long userId;
     private final String username;
+    private final String password;
     private final String role;
     private final String gender;
     private final Integer age;
     private final String address;
 
-    private UserSearchInternalResponse(Long userId, String username, String role, String gender,
-        Integer age, String address) {
+    private UserSearchInternalResponse(
+        Long userId,
+        String username,
+        String password,
+        String role,
+        String gender,
+        Integer age,
+        String address
+    ) {
         this.userId = userId;
         this.username = username;
+        this.password = password;
         this.role = role;
         this.gender = gender;
         this.age = age;
         this.address = address;
     }
 
-    public static UserSearchInternalResponse of(Long userId, String username, String role,
+    public static UserSearchInternalResponse of(
+        Long userId,
+        String username,
+        String password,
+        String role,
         String gender,
-        Integer age, String address) {
+        Integer age,
+        String address
+    ) {
         return new UserSearchInternalResponse(
             userId,
             username,
+            password,
             role,
             gender,
             age,
